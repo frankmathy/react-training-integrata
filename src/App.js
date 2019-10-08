5f3f8f2126c9ea8ae9dfcfbc0e0d99dda11fc5b4
Frank Mathy <frankmathy@gmail.com>
2019-10-08 11:05:41 +0200

    Refactored app code into PeopleAppComponent

--- src/App.js
+++ src/App.js
@@ -1,40 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import PeopleHeaderComponent from "./main/component/PeopleHeaderComponent";
 import PeopleFooterComponent from './main/component/PeopleFooterComponent'
-import PersonComponent from './people/component/PersonComponent'
-import { testPeople, userProfile } from './people/model/TestPeople'
-import PeopleComponent from './people/component/PeopleComponent';
+import PeopleAppComponent from  './people/component/PeopleAppComponent'
 
-class App extends Component {
-  state = {people: testPeople, userProfile: userProfile}
-
-  constructor() {
-    super()
-    setTimeout(() => {
-      userProfile.lastname = "Hoffmann"
-      console.log("AFTER TIMEOUT: " + userProfile.info())
-      this.setState({ userProfile: userProfile})
-    }, 5000)
-  }
-
-  componentDidMount() {
-    let peopleState = {people: testPeople, userProfile: userProfile}
-    this.setState(peopleState)
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <PeopleHeaderComponent/>
-        <div className="App-currentUser">
-          Aktueller User: <PersonComponent person={this.state.userProfile} detail={false}/>
-        </div>
-        <PeopleComponent people={this.state.people}/>
-        <PeopleFooterComponent/>
-      </div>
-    );
-  }
+function App () {
+  return (
+    <div className="App">
+      <PeopleHeaderComponent/>
+      <PeopleAppComponent/>
+      <PeopleFooterComponent/>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/people/component/PeopleAppComponent.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import '../../App.css';
+import PersonComponent from './PersonComponent'
+import PeopleComponent from './PeopleComponent';
+import { testPeople, userProfile } from '../model/TestPeople'
+
+class PeopleAppComponent extends Component {
+  state = {people: testPeople, userProfile: userProfile}
+
+  constructor() {
+    super()
+    setTimeout(() => {
+      userProfile.lastname = "Hoffmann"
+      console.log("AFTER TIMEOUT: " + userProfile.info())
+      this.setState({ userProfile: userProfile})
+    }, 5000)
+  }
+
+  componentDidMount() {
+    let peopleState = {people: testPeople, userProfile: userProfile}
+    this.setState(peopleState)
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="App-currentUser">
+          Aktueller User: <PersonComponent person={this.state.userProfile} detail={false}/>
+        </div>
+        <PeopleComponent people={this.state.people}/>
+      </div>
+    );
+  }
+}
+
+export default PeopleAppComponent;
