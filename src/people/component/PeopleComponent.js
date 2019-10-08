8353032644a76f380462b4073387a7354d8d9e5e
Frank Mathy <frankmathy@gmail.com>
2019-10-08 16:29:37 +0200

    Implemented React table

--- src/people/component/PeopleComponent.js
+++ src/people/component/PeopleComponent.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import PersonComponent from './PersonComponent'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
+import Table from 'react-bootstrap/Table'
 
 let PeopleComponent = (props) => {
     const allPeople = props.people.all().map(person =>
         <PersonComponent person={person} detail={true}/>
     );
     return (
-        <Container>
-            {allPeople}
-        </Container>
+        <Table striped bordered hover>
+            <thead>
+                <tr>
+                    <th>Last Name</th>
+                    <th>First Name</th>
+                    <th>Gender</th>
+                    <th>Size</th>
+                </tr>
+            </thead>
+            <tbody>
+                {allPeople}
+            </tbody>
+        </Table>
     )
 }
 
--- src/people/component/PersonComponent.js
+++ src/people/component/PersonComponent.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
 
 let PersonComponent = (props) => {
-    var personString = ""
     if(props.detail) {
         return (
-            <Row>
-                <Col>{props.person.lastname}</Col>
-                <Col>{props.person.firstname}</Col>
-                <Col>{props.person.gender}</Col>
-                <Col>{props.person.height}</Col>
-            </Row>
+            <tr>
+                <td>{props.person.lastname}</td>
+                <td>{props.person.firstname}</td>
+                <td>{props.person.gender}</td>
+                <td>{props.person.height}</td>
+            </tr>
         )
     } else {
-        personString = props.person.firstname + " " + props.person.lastname
+        return (
+            <tr>
+                <td>{props.person.lastname}</td>
+                <td>{props.person.firstname}</td>
+            </tr>
+        )
     }
-    return (
-        <Col>{personString}</Col>
-    );
 }
 
 export default PersonComponent
