ec9ba485c0c77941b613dc42a5562415109cecd9
Frank Mathy <frankmathy@gmail.com>
2019-10-08 10:43:30 +0200

    Added state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import PeopleHeaderComponent from "./main/component/PeopleHeaderComponent";
 import PeopleFooterComponent from './main/component/PeopleFooterComponent'
@@ -6,17 +6,35 @@ import PersonComponent from './people/component/PersonComponent'
 import { testPeople, userProfile } from './people/model/TestPeople'
 import PeopleComponent from './people/component/PeopleComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <PeopleHeaderComponent/>
-      <div className="App-currentUser">
-        Aktueller User: <PersonComponent person={userProfile} detail={false}/>
+class App extends Component {
+  state = {people: testPeople, userProfile: userProfile}
+
+  constructor() {
+    super()
+    setTimeout(() => {
+      userProfile.lastname = "Hoffmann"
+      console.log("AFTER TIMEOUT: " + userProfile.info())
+      this.setState({ userProfile: userProfile})
+    }, 5000)
+  }
+
+  componentDidMount() {
+    let peopleState = {people: testPeople, userProfile: userProfile}
+    this.setState(peopleState)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <PeopleHeaderComponent/>
+        <div className="App-currentUser">
+          Aktueller User: <PersonComponent person={this.state.userProfile} detail={false}/>
+        </div>
+        <PeopleComponent people={this.state.people}/>
+        <PeopleFooterComponent/>
       </div>
-      <PeopleComponent people={testPeople}/>
-      <PeopleFooterComponent/>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
